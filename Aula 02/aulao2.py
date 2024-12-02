b24e7d80f33e4d982b827c8726a11a9a8ad05ca3
grazitasn <grazielesantosnorberto@gmail.com>
2024-12-02 10:00:24 -0300

    Aprendi o que é uma variavel

---
+++ Aula 02/aulao2.py
@@ -0,0 +1,8 @@
+print("Olá, mundo")
+
+nome_usuario = 'Graziele dos Santos' # tipo -> str = string 
+idade_usuario = 17 # tipo -> int = integer
+altura_usuario = 1. # tipo -> float 
+maior_de_idade = False # tipo -> bool = boolenan
+
+print('sejsa bem-vindo', nome_usuario, 'sua idade é', idade_usuario, 'sua altura é', altura_usuario, 'você é maior deidade?', maior_de_idade)
